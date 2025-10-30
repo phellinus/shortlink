fdb406c045076c0239543182d80f6235d0c0b2fb
桑榆 <2249736997@qq.com>
2025-10-30 20:12:02 +0800

    feat:获取用户信息接口

--- admin/src/main/java/org/sangyu/shortlink/admin/controller/UserController.java
+++ admin/src/main/java/org/sangyu/shortlink/admin/controller/UserController.java
@@ -1,5 +1,8 @@
 package org.sangyu.shortlink.admin.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.sangyu.shortlink.admin.dto.resp.UserRespDTO;
+import org.sangyu.shortlink.admin.service.UserService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,9 +11,13 @@ import org.springframework.web.bind.annotation.RestController;
  * 用户管理控制层
  */
 @RestController
+@RequiredArgsConstructor
 public class UserController {
+
+    private final UserService userService;
+
     @GetMapping("/api/shortlink/v1/user/{username}")
-    public String getUserByUsername(@PathVariable("username") String username) {
-        return "hello! " + username;
+    public UserRespDTO getUserByUsername(@PathVariable("username") String username) {
+        return userService.getUserByUsername(username);
     }
 }
--- admin/src/main/java/org/sangyu/shortlink/admin/service/UserService.java
+++ admin/src/main/java/org/sangyu/shortlink/admin/service/UserService.java
@@ -1,4 +1,17 @@
 package org.sangyu.shortlink.admin.service;
 
-public interface UserService {
+import com.baomidou.mybatisplus.extension.service.IService;
+import org.sangyu.shortlink.admin.dao.entity.UserDO;
+import org.sangyu.shortlink.admin.dto.resp.UserRespDTO;
+
+/**
+ * 用户接口层
+ */
+public interface UserService extends IService<UserDO> {
+    /**
+     * 根据用户名查询用户信息
+     * @param username 用户名
+     * @return 用户返回实体
+     */
+    UserRespDTO getUserByUsername(String username);
 }
