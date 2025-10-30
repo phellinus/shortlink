8f07eee11f3ee6976742cd914035727b1a943fa4
桑榆 <2249736997@qq.com>
2025-10-30 20:43:55 +0800

    feat:Result实体类的封装

--- admin/src/main/java/org/sangyu/shortlink/admin/controller/UserController.java
+++ admin/src/main/java/org/sangyu/shortlink/admin/controller/UserController.java
@@ -1,6 +1,7 @@
 package org.sangyu.shortlink.admin.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.sangyu.shortlink.admin.common.convention.result.Result;
 import org.sangyu.shortlink.admin.dto.resp.UserRespDTO;
 import org.sangyu.shortlink.admin.service.UserService;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,7 +18,12 @@ public class UserController {
     private final UserService userService;
 
     @GetMapping("/api/shortlink/v1/user/{username}")
-    public UserRespDTO getUserByUsername(@PathVariable("username") String username) {
-        return userService.getUserByUsername(username);
+    public Result<UserRespDTO> getUserByUsername(@PathVariable("username") String username) {
+        UserRespDTO result = userService.getUserByUsername(username);
+        if (result == null) {
+            return new Result<UserRespDTO>().setCode("-1").setMessage("用户不存在");
+        }else{
+            return new Result<UserRespDTO>().setCode("0").setData(result);
+        }
     }
 }
