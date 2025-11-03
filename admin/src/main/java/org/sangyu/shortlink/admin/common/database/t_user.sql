f939e496eaa59becefb30b5906a7a2ee28dc588e
桑榆 <2249736997@qq.com>
2025-11-03 17:19:25 +0800

    feat:数据库文件

---
+++ admin/src/main/java/org/sangyu/shortlink/admin/common/database/t_user.sql
@@ -0,0 +1,46 @@
+-- -------------------------------------------------------------
+-- TablePlus 6.0.0(550)
+--
+-- https://tableplus.com/
+--
+-- Database: link
+-- Generation Time: 2025-11-03 17:16:27.2150
+-- -------------------------------------------------------------
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+DROP TABLE IF EXISTS `t_user`;
+CREATE TABLE `t_user` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'ID',
+  `username` varchar(256) DEFAULT NULL COMMENT '用户名',
+  `password` varchar(512) DEFAULT NULL COMMENT '密码',
+  `real_name` varchar(256) DEFAULT NULL COMMENT '真实姓名',
+  `phone` varchar(128) DEFAULT NULL COMMENT '手机号',
+  `mail` varchar(512) DEFAULT NULL COMMENT '邮箱',
+  `deletion_time` bigint(20) DEFAULT NULL COMMENT '注销时间戳',
+  `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+  `update_time` datetime DEFAULT NULL COMMENT '修改时间',
+  `del_flag` tinyint(1) DEFAULT NULL COMMENT '删除标识0:未删除 1:已删除',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
+
+INSERT INTO `t_user` (`id`, `username`, `password`, `real_name`, `phone`, `mail`, `deletion_time`, `create_time`, `update_time`, `del_flag`) VALUES
+(1, 'zhuxukai', '123456', '桑榆', '15005871200', 'sangyu20021007@gmail.com', NULL, '2025-10-30 11:46:57', '2025-10-30 11:46:57', 0);
+
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
