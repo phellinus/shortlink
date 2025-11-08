ed4aa4ebbd1f9cdeaae02076c185a2fab7afeabf
桑榆 <2249736997@qq.com>
2025-11-08 16:11:18 +0800

    feat:通过分布式锁，防止用户用相同用户名发起恶意请求

---
+++ admin/src/main/java/org/sangyu/shortlink/admin/common/constant/RedisCacheConstant.java
@@ -0,0 +1,8 @@
+package org.sangyu.shortlink.admin.common.constant;
+
+/**
+ * 短链接后台管理 Redis 缓存常量类
+ */
+public class RedisCacheConstant {
+    public static final String LOCK_USER_REGISTER_KEY = "short-link:lock_user-register:";
+}
--- admin/src/main/java/org/sangyu/shortlink/admin/service/impl/UserServiceImpl.java
+++ admin/src/main/java/org/sangyu/shortlink/admin/service/impl/UserServiceImpl.java
@@ -6,6 +6,8 @@ import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.redisson.api.RBloomFilter;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
 import org.sangyu.shortlink.admin.common.convention.exception.ClientException;
 import org.sangyu.shortlink.admin.common.enums.UserErrorCodeEnum;
 import org.sangyu.shortlink.admin.dao.entity.UserDO;
@@ -16,6 +18,8 @@ import org.sangyu.shortlink.admin.service.UserService;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
+import static org.sangyu.shortlink.admin.common.constant.RedisCacheConstant.LOCK_USER_REGISTER_KEY;
+
 /**
  * 用户接口实现层
  */
@@ -24,6 +28,7 @@ import org.springframework.stereotype.Service;
 public class UserServiceImpl extends ServiceImpl<UserMapper, UserDO> implements UserService {
 
     private final RBloomFilter<String> userRegisterCachePenetrationBloomFilter;
+    private final RedissonClient redissonClient;
 
     @Override
     public UserRespDTO getUserByUsername(String username) {
@@ -49,10 +54,19 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, UserDO> implements 
         if (!hasUsername(requestParam.getUsername())) {
             throw new ClientException(UserErrorCodeEnum.USER_NAME_EXIT);
         }
-        int insert = baseMapper.insert(BeanUtil.toBean(requestParam, UserDO.class));
-        if(insert<1){
-            throw new ClientException(UserErrorCodeEnum.USER_SAVE_ERROR);
+        RLock lock = redissonClient.getLock(LOCK_USER_REGISTER_KEY+requestParam.getUsername());
+        try {
+            if (lock.tryLock()) {
+                int insert = baseMapper.insert(BeanUtil.toBean(requestParam, UserDO.class));
+                if(insert<1){
+                    throw new ClientException(UserErrorCodeEnum.USER_SAVE_ERROR);
+                }
+                userRegisterCachePenetrationBloomFilter.add(requestParam.getUsername());
+                return;
+            }
+            throw new ClientException(UserErrorCodeEnum.USER_NAME_EXIT);
+        } finally {
+            lock.unlock();
         }
-        userRegisterCachePenetrationBloomFilter.add(requestParam.getUsername());
     }
 }
